65d91f6750755da2c8edc4c2fb9df035fefd2fd9
tijinator <tijbedi@gmail.com>
2016-01-21 15:36:09 -0500

    Trying to fix error with angular minification

--- backend/app/assets/javascripts/course/course_service.js
+++ backend/app/assets/javascripts/course/course_service.js
@@ -2,17 +2,6 @@ angular.module('lltv')
 .service('CourseService', [
 '$http',
 function($http) {
-  this.getFeaturedCourse = function(id) {
-    var url;
-    if (id) {
-      url = '/api/categories/featured_course' + '?Course_id=' + id;
-    } else {
-      url = '/api/categories/featured_course'
-    }
-
-    return $http.get(url);
-  };
-
   this.getCourses = function() {
     return $http.get('/api/courses');
   };
@@ -21,7 +10,4 @@ function($http) {
     return $http.get('/api/courses/' + id);
   };
 
-  /*this.getCourseCourses = function(id) {
-    return $http.get('/api/categories/' + id + '/courses');
-  };*/
 }]);
