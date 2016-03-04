13c8da9069f21c3d62c6ea88cab4b33d6f2199b5
Rovshen Gurdov <rgurdov@gmail.com>
2016-03-04 14:57:31 -0500

    category serializer fixed

--- backend/app/serializers/category_serializer.rb
+++ backend/app/serializers/category_serializer.rb
@@ -1,6 +1,6 @@
 class CategorySerializer < ActiveModel::Serializer
   
-  attributes :id
+  attributes :id, :user_id, :parent_id, :title, :details, :position, :image_file_name, :image_content_type, :image_file_size,
+  :image_updated_at, :html_tab_id, :html_tab_color, :html_color, :homepage_position, :small_image_url, :banner_url
   
-  # has_many :courses
 end
