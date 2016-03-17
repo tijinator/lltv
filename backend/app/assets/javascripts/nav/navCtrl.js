9d21b84c2ee5d30b045ef86982cb053646ce22f5
Martin Ben <mebenav88@gmail.com>
2016-03-17 16:30:09 -0400

    set html profile nav option under profile name

--- backend/app/assets/javascripts/nav/navCtrl.js
+++ backend/app/assets/javascripts/nav/navCtrl.js
@@ -7,13 +7,71 @@ angular.module('lltv')
 'currentUser',
 function($rootScope, $scope, $location, $auth, currentUser) {
 
+  $scope.callMe = function() {
+    console.log('yeah i work');
+      profileMenu();
+  }
+
+  var profileMenu = function(){
+
+    // var profileOptions = ["account settings", "profile", "help", "contact us", "sign out"];
+    // var li = document.createElement('li');
+    // var ul = document.createElement('ul');
+
+    // var domList = document.getElementsByClassName("call-me");
+    var html = `
+    <a class="dropdown-toggle" data-toggle="dropdown"> Profile</a>
+    <ul class="dropdown-menu" role="menu">
+      <li><a href="/categories" style="color: #000;">account settings</a></li>
+      <li><a href="/courses/1" style="color: #000;">profile</a></li>
+      <li><a href="/categories/1" style="color: #000;">help</a></li>
+      <li><a href="/categories" style="color: #000;">contact us</a></li>
+      <li><a href="/courses/1" style="color: #000;">sign out</a></li>
+    </ul>
+    `
+
+    $('.call-me').html(html);
+    // domList.appendChild(html);
+
+    console.log(html);
+
+
+
+    //set attributes to li => class="dropdown-toggle" data-toggle="dropdown"
+    // li.setAttribute('class', 'profile-nav-menu dropdown-toggle');
+    // li.setAttribute('data-toggle', 'dropdown');
+    //
+    // //set attributes class, role to ul -> class="dropdown-menu" role="menu"
+    // ul.setAttribute('class', 'dropdown-menu');
+    // ul.setAttribute('role', 'menu');
+
+    //set li childrent to ul
+    // for(var i = 0; i < profileOptions.length; i++){
+    //   // var text = document.createTextNode("Water");
+    //   var ulChildrenLi = document.createElement('li');
+    //   ulChildrenLi.innerHTML = profileOptions[i];
+    //   ul.appendChild(ulChildrenLi);
+    // }
+    // // ul.appendChild(ulChildren);
+    // // console.log(ulChildrenLi);
+    // li.appendChild(ul);
+
+
+
+
+    // var domList = document.getElementsByClassName("profile-nav-menu");
+    // var $list = $(domList); //convert to jquery element
+    // var $ul = $('<ul class="dropdown-menu"></ul>');
+    // var $res = $list.append($ul);
+  }
+
   $scope.isActive = function (route){
     return (route == $location.path()) ? true : false
   }
 
   $scope.signedIn = function() {
     // return currentUser.username;
-    return $scope.user.first_name;
+    return "Hi, " + $scope.user.first_name;
   }
 
   $scope.isLoggedIn = function() {
