83631f6cecc0dd36228b121359060878fb89f3f0
Martin Ben <mebenav88@gmail.com>
2016-03-27 13:49:51 -0400

    add scroll javascript in homepage html, file.  Adjust top position when loading new page

--- backend/app/assets/javascripts/homepage_colored_bar_scroll.js
+++ backend/app/assets/javascripts/homepage_colored_bar_scroll.js
@@ -1,16 +1,15 @@
-var coloredBarScroll = function(){
-
-  $("#category-button div").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".colored-bar").offset().top - 103
-        // scrollTop: $(document).height() * 0.753 - $(window).height()
-    }, 600, "easeOutCirc", function(){
-      console.log("scroll!");
-    });
-  });
-
-}
-
-$(document).ready(coloredBarScroll);
-$(window).bind('page:change', coloredBarScroll);
-$(window).bind('unload', coloredBarScroll);
+// var coloredBarScroll = function(){
+//   $("#category-button div").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $(".colored-bar").offset().top - 103
+//         // scrollTop: $(document).height() * 0.753 - $(window).height()
+//     }, 600, "easeOutCirc", function(){
+//       console.log("scroll!");
+//     });
+//   });
+//
+// }
+//
+// $(document).ready(coloredBarScroll);
+// $(window).bind('page:change', coloredBarScroll);
+// $(window).bind('unload', coloredBarScroll);
