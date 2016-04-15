91e7e2bc5cf315692e2f8e0f1254c578922889a4
Martin Ben <mebenav88@gmail.com>
2016-04-14 21:40:01 -0400

    add create action to chapter in admin

--- backend/app/controllers/admin/chapter_admin_controller.rb
+++ backend/app/controllers/admin/chapter_admin_controller.rb
@@ -17,10 +17,17 @@ class Admin::ChapterAdminController < Admin::AdminController
     render 'admin/chapter', layout: 'sb2'
   end
 
+  def new
+    @chapter = Chapter.new
+  end
+
   def create
-    parent_course = Chapter.find(params[:course_id])
-    parent_course.chapters.create!(chapter_params.merge({user_id: current_user.id}))
-    redirect_to course_chapters_path(parent_chapter)
+    # parent_course = Chapter.find(params[:course_id])
+    # parent_course.chapters.create!(chapter_params.merge({user_id: current_user.id}))
+    # redirect_to course_chapters_path(parent_chapter)
+    @course = Course.find(params[:course_id])
+    @chapter = @course.chapters.create(chapter_params.merge({user_id: current_user.id}))
+    redirect_to @course
   end
 
   private
