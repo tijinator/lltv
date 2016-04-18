a95d7656b8366e2fb95c1e779064c7e984594815
Rovshen Gurdov <rgurdov@gmail.com>
2016-04-18 19:51:46 -0400

    /carrierwave.rb/

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@ if Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = :public_read
-    config.asset_host = ENV.fetch('S3_HOST')
+    # config.asset_host = ENV.fetch('S3_HOST')
     # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
     config.aws_credentials = {
