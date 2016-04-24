f965cc701dd8a6de254277f30ca9e2ece41ee5df
Martin Ben <mebenav88@gmail.com>
2016-04-24 16:46:51 -0400

    add refresh function after updating email

--- backend/app/assets/javascripts/settingAccount/accountSettingCtrl.js
+++ backend/app/assets/javascripts/settingAccount/accountSettingCtrl.js
@@ -6,17 +6,16 @@ angular.module('lltv')
 '$auth',
 '$cookieStore',
 '$location',
-// '$route',
-function($rootScope, $scope, currentUser, $auth, $cookieStore, $location){
-  var userObj = currentUser.getUserObj();
-  //
+'$window',
+function($rootScope, $scope, currentUser, $auth, $cookieStore, $location, $window){
+  var userObj = currentUser.getUserObj(); //get currentUser object
+
   $scope.userEmail = userObj.email; //send current email to update form
 
   $scope.updateAccount = function() {
      $auth.updateAccount($scope.updateAccountForm)
        .then(function(resp) {
          // handle success response
-
        })
        .catch(function(resp) {
          // handle error response
@@ -27,9 +26,6 @@ function($rootScope, $scope, currentUser, $auth, $cookieStore, $location){
 
       $cookieStore.remove('userObj'); //remove current userObj cookie
       $cookieStore.put('userObj', userData.data); //update userObj cookie
-
-      // $location.path("/account");
-      // $route.reload();
-
+      $window.location.reload(); //refresh page
    });
 }]);
