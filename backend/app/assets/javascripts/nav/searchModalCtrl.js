77dd00f2092d45806dbe8419a91c62a9b72b7ea1
Martin Ben <mebenav88@gmail.com>
2016-05-14 20:32:01 -0400

    add autocomplete css

--- backend/app/assets/javascripts/nav/searchModalCtrl.js
+++ backend/app/assets/javascripts/nav/searchModalCtrl.js
@@ -4,7 +4,8 @@ angular.module('lltv')
 'searchModalService',
 'CategoryService',
 'searchCourseService',
-function($scope, searchModalService, CategoryService, searchCourseService){
+'$http',
+function($scope, searchModalService, CategoryService, searchCourseService, $http){
 
   $scope.openSearch = function(){
     searchModalService.openModal();
@@ -14,7 +15,32 @@ function($scope, searchModalService, CategoryService, searchCourseService){
     searchModalService.closeModal();
   }
 
+  // $scope.searchCourses = function(val){
+  //   return $http.get('api/courses')
+  //   .then(function(response){
+  //     return response.data;
+  //   });
+  // }
+  $scope.selected = undefined;
 
+  $scope.searchCourses = [];
+
+  $http.get('api/courses')
+    .then(function(response){
+      $scope.searchCourses = response.data;
+
+      // $scope.startsWith = function(state, viewValue) {
+      //   return state.substr(0, viewValue.length).toLowerCase() == viewValue.toLowerCase();
+      // }
+
+    });
+
+
+
+  // console.log($scope.searchCourses);
+
+
+// another way to do this but w/ two services
   // CategoryService.getCategories().then(function(obj){
   //
   //   var categoryArr = [];
@@ -35,27 +61,24 @@ function($scope, searchModalService, CategoryService, searchCourseService){
   //   }
   // });
 
-  searchCourseService.getCourses().then(function(obj){
-    // $scope.searchCategories = obj.data;
-
-    var coursesArr = [];
-    var courseArrObj = obj.data;
-
-    for(var i = 0; i < courseArrObj.length; i++){
-
-      coursesArr.push(courseArrObj[i].title);
-    }
-
-    $scope.complete=function(){
-      console.log(coursesArr);
-      $( "#tags" ).autocomplete({
-        source: coursesArr,
-        minLength: 2
-      });
-    }
-  });
-
-  // console.log($scope.searchCategories);
+  // searchCourseService.getCourses().then(function(obj){
+  //
+  //   var coursesArr = [];
+  //   var courseArrObj = obj.data;
+  //
+  //   for(var i = 0; i < courseArrObj.length; i++){
+  //
+  //     coursesArr.push(courseArrObj[i].title);
+  //   }
+  //
+  //   $scope.complete=function(){
+  //     console.log(coursesArr);
+  //     $( "#tags" ).autocomplete({
+  //       source: coursesArr,
+  //       minLength: 2
+  //     });
+  //   }
+  // });
 
 }
 ]);
