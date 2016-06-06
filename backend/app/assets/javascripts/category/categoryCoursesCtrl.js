dee1cf2e351f8839dde4cd7cc0877b1d9190dc36
Martin Ben <mebenav88@gmail.com>
2016-06-06 18:49:56 -0400

    add autoscroll to category page mobile

--- backend/app/assets/javascripts/category/categoryCoursesCtrl.js
+++ backend/app/assets/javascripts/category/categoryCoursesCtrl.js
@@ -4,26 +4,42 @@ angular.module('lltv')
 'category',
 'CategoryService',
 function($scope, category, CategoryService) {
-  	$scope.category_details = category.data;
+	$scope.category_details = category.data;
+
 	$scope.getSubs = function(id){
-		category.data.subcategory.forEach(function(sub) {
-			if (sub.id === id){ $scope.subcategory = sub.title }
-		});
+    category.data.subcategory.forEach(function(sub) {
+    	if (sub.id === id){ $scope.subcategory = sub.title }
+    });
 
-		CategoryService.getCategoryCourses(id).then(function(courses) {
-			$scope.$broadcast( 'subcategory', courses);
-		});
+    CategoryService.getCategoryCourses(id).then(function(courses) {
+    	$scope.$broadcast( 'subcategory', courses);
+    });
+
+    scrollToCourses();
+  }
+
+  $scope.all_courses = function(id){
+    // category.data.subcategory.forEach(function(sub) {
+    // 	if (sub.id === id){ $scope.subcategory = sub.title }
+    // });
+    $scope.subcategory = 'All';
+
+    CategoryService.getCategoryCourses(id).then(function(courses) {
+    	$scope.$broadcast( 'subcategory', courses);
+    });
+
+    scrollToCourses();
+
+  }
+
+  function scrollToCourses(){
+    if ($(window).width() < 767) {
+
+      $('html, body').animate({
+          scrollTop: $(".category-trending-courses").offset().top - 95
+      }, 600, "easeOutCirc", function(){});
+
     }
-
-    $scope.all_courses = function(id){
-		// category.data.subcategory.forEach(function(sub) {
-		// 	if (sub.id === id){ $scope.subcategory = sub.title }
-		// });
-		$scope.subcategory = 'All';
-
-		CategoryService.getCategoryCourses(id).then(function(courses) {
-			$scope.$broadcast( 'subcategory', courses);
-		});
-    }
+  }
 
 }]);
